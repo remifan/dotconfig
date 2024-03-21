cd5bbdf8a56087f051ba961a1e11e9e481267eb3
Remi Fan <remi@rqrfmba4work.local>
2024-03-21 15:16:44 +0800

    add firenvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -305,5 +305,38 @@ require("lazy").setup({
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
   },
+  {
+    'glacambre/firenvim',
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    lazy = not vim.g.started_by_firenvim,
+    build = function()
+        vim.fn["firenvim#install"](0)
+    end,
+    init = function ()
+      vim.api.nvim_create_autocmd({'UIEnter'}, {
+        callback = function(event)
+          local client = vim.api.nvim_get_chan_info(vim.v.event.chan).client
+            if client ~= nil and client.name == "Firenvim" then
+              vim.o.laststatus = 0
+            end
+        end
+      })
+    end,
+    config = function()
+      vim.g.firenvim_config = {
+        globalSettings = { alt = "all" },
+        localSettings = {
+            [".*"] = {
+                cmdline  = "firenvim",
+                content  = "text",
+                priority = 0,
+                selector = "textarea",
+                takeover = "never"
+            }
+        }
+      }
+    end,
+  },
 })
 
