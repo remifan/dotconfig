19b26cd1cd3450833fee553891b2c92c9717d554
Remi QR FAN <remi.qr.fan@gmail.com>
2026-03-07 18:43:24 +0800

    add lazygit.nvim integration

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -12,4 +12,14 @@ return {
       require('config.git')
     end,
   },
+
+  -- Lazygit integration (full git UI in a floating terminal)
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+    },
+  },
 }
